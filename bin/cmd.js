1f4f5eab734f78975ca3b6f5fcc00dc6c3d3b3d8
bthesorceror <bthesorceror@gmail.com>
2013-09-19 13:48:07 -0400

    Simplier api for syncing

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,16 +2,24 @@
 var PotatoChip = require('../index');
 var path = require('path');
 
-var url      = process.argv[2];
-var doc_file = path.resolve(process.cwd(), process.argv[3]);
-var name     = process.argv[4];
+var file = require(path.resolve(process.cwd(), process.argv[2]));
 
-var chip = new PotatoChip(url);
+var chip = new PotatoChip(file.url);
 
-chip.updateDesign(require(doc_file), name, function(err) {
+chip.updateDesign(file.doc, file.name, function(err) {
   if (err) {
     console.log("Your potato failed", err);
     return;
   }
   console.log("All updated now!");
 });
+
+/*
+*
+* module.exports = {
+*   name: 'test',
+*   url: 'http://local:host@localhost:5984/hashrocket_stats',
+*   doc: {
+*
+*   }
+* }
